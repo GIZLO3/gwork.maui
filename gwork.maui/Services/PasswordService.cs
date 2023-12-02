2f27da432fb6ce9c416ff6eaa2299a1ad0c0e0cd
Gizlo3 <m.giza12345@gmail.com>
2023-12-02 15:13:15 +0100

    added PasswordService

---
+++ gwork.maui/Services/PasswordService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gwork.maui.Services
+{
+    public class PasswordService
+    {
+        private const int keySize = 64;
+        private const int iterations = 350000;
+        private static HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+        public static string HashPasword(string password, out byte[] salt)
+        {
+            salt = RandomNumberGenerator.GetBytes(keySize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, iterations, hashAlgorithm, keySize);
+            return Convert.ToHexString(hash);
+        }
+        public static bool VerifyPassword(string password, string hash, byte[] salt)
+        {
+            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
+            return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(hash));
+        }
+    }
+}
